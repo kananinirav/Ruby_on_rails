bcaf816f98477f311401a59ac38dbd1e682179a6
niravkanani <kananinirav0@gmail.com>
2019-03-07 23:21:25 +0530

    user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
-
-end  
+  validates :username, presence: true,
+             uniqueness: {case_sensitive: false},
+             length: {minimum: 3, maximum: 20}
+  VALID_EMAIL_REGEX = /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/i
+  validates :email,presence: true,
+             uniqueness: { case_sensitive: false},
+             length: { maximum: 100},
+             format: { with: VALID_EMAIL_REGEX}
+end
