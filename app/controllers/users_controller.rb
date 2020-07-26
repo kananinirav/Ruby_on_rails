49af58815886e509b9907dd78388d7ab8a900433
kananinirav <kananinirav0@gmail.com>
2020-07-26 08:16:42 +0530

    about page change and limit change

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   before_action :require_admin, only: [:destroy]
 
   def index
-    @users = User.paginate(page: params[:page], per_page: 5)
+    @users = User.paginate(page: params[:page], per_page: 15)
   end
   def new
     @user = User.new
